947ea0cb3eabdd98c37f96186153572b9113f5ef
vanyasree123 <vinnusaruvu@gmail.com>
2025-01-29 05:37:47 -0800

    Add files via upload

---
+++ sumofeven.java
@@ -0,0 +1,10 @@
+public class sumofeven {
+    public static void main (String[] args){
+        int result=0;
+        for(int i =0;i<=100;i=i+2){
+            result=result+i;
+            System.out.println(result);
+        }
+    }
+    
+}
